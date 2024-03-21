f17202e7c14496e7305189d9b151544304a9b751
Luciano <lpuzer@yahoo.com.br>
2024-03-21 13:45:35 -0300

    Initial Commit

---
+++ ToDoFirebase/ToDoFirebaseApp.swift
@@ -0,0 +1,12 @@
+//Created by Halbus Development
+
+import SwiftUI
+
+@main
+struct ToDoFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
