b272308ecf701fb826f6dd4cfb9231c1dbb53585
Luciano <lpuzer@yahoo.com.br>
2024-03-24 21:57:44 -0300

    Adding Filters and Sorts

--- ToDoFirebase/Core/Products/ProductsView.swift
+++ ToDoFirebase/Core/Products/ProductsView.swift
@@ -5,10 +5,55 @@ import SwiftUI
 @MainActor
 final class ProductsViewModel: ObservableObject {
     @Published private(set) var products: [Product] = []
+    @Published var selectedFileter: FilterOption? = nil
+    @Published var selectedCategory: CategoryOption? = nil
     
-    func getAllProducts() async throws {
-        self.products = try await ProductsManager.share.getAllProducts()
+    enum FilterOption: String, CaseIterable {
+        case noFilter
+        case priceHigh
+        case priceLow
+        
+        var priceDescending: Bool? {
+            switch self {
+            case .noFilter: return nil
+            case .priceHigh: return true
+            case .priceLow: return false
+            }
+        }
     }
+    
+    func filterSelected(option: FilterOption) async throws {
+        self.selectedFileter = option
+        self.getProducts()
+    }
+    
+    enum CategoryOption: String, CaseIterable {
+        case noCategory
+        case smartphones
+        case laptops
+        case fragrances
+        
+        var categoryKey: String? {
+            if self == .noCategory {
+                return nil
+            } else {
+                return self.rawValue
+            }
+        }
+    }
+    
+    func categorySelected(option: CategoryOption) async throws {
+        self.selectedCategory = option
+        self.getProducts()
+        
+    }
+    
+    func getProducts() {
+        Task {
+            self.products = try await ProductsManager.share.getAllProducts(priceDescending: selectedFileter?.priceDescending, forCategory: selectedCategory?.categoryKey)
+        }
+    }
+ 
 }
 
 struct ProductsView: View {
@@ -20,8 +65,34 @@ struct ProductsView: View {
                 ProductsCellView(product: product)
             }
         }
-        .task {
-            try? await viewModel.getAllProducts()
+        .navigationTitle("Products")
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                Menu("Filter: \(viewModel.selectedFileter?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.FilterOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.filterSelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Menu("Category: \(viewModel.selectedCategory?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.CategoryOption.allCases, id: \.self) { option in
+                        Button(option.rawValue) {
+                            Task {
+                                try? await viewModel.categorySelected(option: option)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        .onAppear {
+            viewModel.getProducts()
         }
     }
 }
